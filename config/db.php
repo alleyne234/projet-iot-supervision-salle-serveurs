48b1816e9c96a57a3050141baf64e3a96202081d
Alexandre Wauquier <alleyne234@outlook.fr>
2024-03-29 23:41:45 +0100

    Ajout système connexion

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+$db_host = 'localhost';
+$db_port = '3306';
+$db_dbname   = 'db1';
+$db_charset = 'utf8mb4';
+$db_username = 'root';
+$db_password = '';
+
+$dsn = "mysql:host=$db_host;port=$db_port;dbname=$db_name;charset=$db_charset";
+
+try {
+     $pdo = new PDO($dsn, $db_username, $db_password);
+     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+     echo "Échec de la connexion : " . $e->getMessage();
+}
+
+?>
