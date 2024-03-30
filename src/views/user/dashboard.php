0326cde8084cc366c47b004f2686dadd7be165f6
Alexandre Wauquier <alleyne234@outlook.fr>
2024-03-30 18:34:18 +0100

    Préparation zones graphiques

--- src/views/user/dashboard.php
+++ src/views/user/dashboard.php
@@ -23,12 +23,14 @@ if (!isset($_SESSION['user_id'])) {
 </head>
 
 <body>
+<!-- Sidebar Toggle -->
 <input type="checkbox" id="sidebar-toggle">
     <label for="sidebar-toggle">
         <i class="fa-solid fa-bars" id="sidebar-btn-open"></i>
         <i class="fa-solid fa-xmark" id="sidebar-btn-close"></i>
     </label>
     
+    <!-- Sidebar -->
     <div class="sidebar">
         <a href="./../../../index.php" class="active">
             <i class="fa-solid fa-house"></i>
@@ -41,6 +43,39 @@ if (!isset($_SESSION['user_id'])) {
     </div>
 
     <main>
+        <div class="dashboard-container">
+            <h1>Valeurs DHT11</h1>
+            
+            <div class="graph-container">
+                <!-- Temperature Charts -->
+                <div class="temperature-container">
+                    <div class="graph temperature-graph">
+                        <canvas id="dashboard-temperature-graph"></canvas>
+                    </div>
+    
+                    <div class="graph temperature-graph">
+                        <canvas id="dashboard-temperature-gauge"></canvas>
+                    </div>
+                </div>
+    
+                <!-- Humidity Charts -->
+                <div class="humidity-container">
+                    <div class="graph humidity-graph">
+                        <canvas id="dashboard-humidity-graph"></canvas>
+                    </div>
+    
+                    <div class="graph humidity-graph">
+                        <canvas id="dashboard-humidity-gauge"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script type="module" src="./../../../public/js/charts/temperature-graph.js"></script>
+    <script type="module" src="./../../../public/js/charts/temperature-gauge.js"></script>
+    <script type="module" src="./../../../public/js/charts/humidity-graph.js"></script>
+    <script type="module" src="./../../../public/js/charts/humidity-gauge.js"></script>
 </body>
 </html>
