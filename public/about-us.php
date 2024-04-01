a4cee2387dbc5989dcfa54414a2f455720c87e01
Alexandre Wauquier <alleyne234@outlook.fr>
2024-04-01 13:20:39 +0200

    Ajout page about-us.php

---
+++ public/about-us.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>À Propos</title>
+    <link rel="icon" href="./img/circle-info-solid.svg">
+    <link rel="stylesheet" href="./css/default.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Back Button -->
+    <a href="./../index.php" id="back-button">
+        <i class="fa-solid fa-backward-step" id="sidebar-btn-open"></i>
+    </a>
+</body>
+</html>
