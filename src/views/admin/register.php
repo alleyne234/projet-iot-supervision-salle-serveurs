c83726648f59508a23170761ab16029eb8b2a110
Alexandre Wauquier <alleyne234@outlook.fr>
2024-04-03 17:23:49 +0200

    Ajout système vérification identifiants nouveau utilisateur

--- src/views/admin/register.php
+++ src/views/admin/register.php
@@ -7,24 +7,57 @@ if (!isset($_SESSION['user_id'])) {
     exit;
 }
 
-include("./../../../config/db.php");
+require_once("./../../../config/db.php");
 
 $message = '';
 
-if (isset($_POST['username']) && isset($_POST['password'])) {
-    $username = $_POST['username'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    $email = $_POST['email'];
-    $is_admin = isset($_POST['is-admin']) ? 1 : 0;
+function validate_username($username) {
+    return preg_match('/^[a-zA-Z][a-zA-Z0-9_.-]{3,29}$/', $username);
+}
 
-    $sql = "INSERT INTO users (email, username, password, is_admin) VALUES (:email, :username, :password, :is_admin)";
-    $stmt = $dbh->prepare($sql);
-    $result = $stmt->execute(['email' => $email, 'username' => $username, 'password' => $password, 'is_admin' => $is_admin]);
+function validate_password($password) {
+    return preg_match('/^[a-zA-Z0-9!"#$%&\'()*+,-.\/:;<=>?@[\\]^_`{|}~]{4,70}$/', $password);
+}
 
-    if ($result) {
-        $message = '<p style="color:green">Inscription réussie</p>';
+// Vérification que la requête est une méthode POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Vérification de la présence des identifiants
+    if (!empty($_POST['email']) && !empty($_POST['username']) && !empty($_POST['password'])) {
+        $email = $_POST['email'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $is_admin = isset($_POST['is-admin']) ? 1 : 0;
+
+        // Vérification des exigences pour les identifiants
+        if (!validate_username($username)) {
+            $message = '<p style="color:red">Le nom d\'utilisateur doit répondre aux exigences suivantes :<br>
+            - Avoir entre 4 et 30 caractères<br>
+            - Peut contenir des lettres, des chiffres<br>
+            - Peut contenir les caractères "_", "." ou "-"
+            </p>';
+        } elseif (!validate_password($password)) {
+            $message = '<p style="color:red">Le mot de passe doit répondre aux exigences suivantes :<br>
+            - Avoir entre 4 et 70 caractères<br>
+            - Peut contenir des lettres et des chiffres<br>
+            - Peut contenir les caractères spéciaux suivants :<br>
+            !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~
+            </p>';
+        } else {
+            $password_hash = password_hash($password, PASSWORD_DEFAULT);
+            
+            // Requête SQL
+            $sql = "INSERT INTO users (email, username, password, is_admin) VALUES (:email, :username, :password, :is_admin)";
+            $stmt = $dbh->prepare($sql);
+            $result = $stmt->execute(['email' => $email, 'username' => $username, 'password' => $password_hash, 'is_admin' => $is_admin]);
+
+            if ($result) {
+                $message = "<p style='color:green'>Inscription réussie</p>";
+            } else {
+                $message = "<p style='color:red'>Erreur lors de l'inscription</p>";
+            }
+        }
     } else {
-        $message = '<p style="color:red">Erreur lors de l\'inscription</p>';
+        $message = "<p style='color:red'>Veuillez saisir une adresse email, un nom d'utilisateur et un mot de passe.</p>";
     }
 }
 ?>
