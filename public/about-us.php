48ccba8a3a5a800ed395034cedafddeaf8b9b7dc
Alexandre Wauquier <alleyne234@outlook.fr>
2024-04-04 21:47:41 +0200

    Ajout du texte page à propos

--- public/about-us.php
+++ public/about-us.php
@@ -10,6 +10,7 @@ session_start();
     <title>À Propos</title>
     <link rel="icon" href="./img/circle-info-solid.svg">
     <link rel="stylesheet" href="./css/default.css">
+    <link rel="stylesheet" href="./css/about-us.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
 </head>
 
@@ -18,5 +19,20 @@ session_start();
     <?php
     include("./../src/includes/sidebar.php");
     ?>
+
+    <main>
+        <div class="about-us-container">
+            <!-- About the project -->
+            <h3>À propos du projet</h3>
+            <p>Le projet consiste à développer un système de surveillance environnementale pour la salle de serveurs de l'entreprise Nomios,
+                afin de garantir des conditions optimales de fonctionnement pour l'équipement informatique.</p>
+
+            <!-- Members -->
+            <h3 class="about-us-members">Membres de l'équipe</h3>
+            <p>- BOUTABA Ianice</p>
+            <p>- KONIECZKOWICZ Raphael</p>
+            <p>- WAUQUIER Alexandre</p>
+        </div>
+    </main>
 </body>
 </html>
