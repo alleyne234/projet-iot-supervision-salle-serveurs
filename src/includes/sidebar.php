93801d7ec0748df01af25f9ad052b9deed161e36
Alexandre Wauquier <alleyne234@outlook.fr>
2024-04-12 19:50:55 +0200

    Ajout du nom de l'utilisateur dans la sidebar

--- src/includes/sidebar.php
+++ src/includes/sidebar.php
@@ -27,53 +27,60 @@ function saveState() {
 
 <!-- Sidebar -->
 <div class="sidebar">
-    <!-- Home -->
-    <a href="/<?php echo $root_folder_name ?>index.php"
-    <?php if ($page_name == "index.php") echo 'class="active-page"'; ?>>
-        <i class="fa-solid fa-house"></i>
-        <span>Accueil</span>
-    </a>
-
-    <!-- Register -->
-    <?php if (isset($_SESSION['user_id']) && $_SESSION['user_is_admin']): ?>
-        <a href="/<?php echo $root_folder_name ?>src/views/register.php"
-        <?php if ($page_name == "register.php") echo 'class="active-page"'; ?>>
-        <i class="fa-regular fa-id-card"></i>
-            <span>Créer compte</span>
-        </a>
+    <!-- Username -->
+    <?php if (isset($_SESSION['user_name'])): ?>
+        <h2><?php echo $_SESSION['user_name'] ?></h2>
     <?php endif; ?>
 
-    <!-- Dashboard -->
-    <?php if (isset($_SESSION['user_id'])): ?>
-        <a href="/<?php echo $root_folder_name ?>src/views/dashboard.php"
-        <?php if ($page_name == "dashboard.php") echo 'class="active-page"'; ?>>
+    <div class="sidebar-links">
+        <!-- Home -->
+        <a href="/<?php echo $root_folder_name ?>index.php"
+        <?php if ($page_name == "index.php") echo 'class="active-page"'; ?>>
+            <i class="fa-solid fa-house"></i>
+            <span>Accueil</span>
+        </a>
+
+        <!-- Register -->
+        <?php if (isset($_SESSION['user_id']) && $_SESSION['user_is_admin']): ?>
+            <a href="/<?php echo $root_folder_name ?>src/views/register.php"
+            <?php if ($page_name == "register.php") echo 'class="active-page"'; ?>>
+            <i class="fa-regular fa-id-card"></i>
+                <span>Créer compte</span>
+            </a>
+        <?php endif; ?>
+
+        <!-- Dashboard -->
+        <?php if (isset($_SESSION['user_id'])): ?>
+            <a href="/<?php echo $root_folder_name ?>src/views/dashboard.php"
+            <?php if ($page_name == "dashboard.php") echo 'class="active-page"'; ?>>
+                <i class="fa-solid fa-circle-info"></i>
+                <span>Dashboard</span>
+            </a>
+        <?php endif; ?>
+
+        <!-- Login -->
+        <?php if (!isset($_SESSION['user_id'])): ?>
+            <a href="/<?php echo $root_folder_name ?>src/views/login.php"
+            <?php if ($page_name == "login.php") echo 'class="active-page"'; ?>>
+                <i class="fa-solid fa-key"></i>
+                <span>Connexion</span>
+            </a>
+        <?php endif; ?>
+
+        <!-- Logout -->
+        <?php if (isset($_SESSION['user_id'])): ?>
+            <a href="/<?php echo $root_folder_name ?>src/views/logout.php"
+            <?php if ($page_name == "logout.php") echo 'class="active-page"'; ?>>
+                <i class="fa-solid fa-right-from-bracket"></i>
+                <span>Déconnexion</span>
+            </a>
+        <?php endif; ?>
+
+        <!-- About us -->
+        <a href="/<?php echo $root_folder_name ?>src/views/about-us.php"
+        <?php if ($page_name == "about-us.php") echo 'class="active-page"'; ?>>
             <i class="fa-solid fa-circle-info"></i>
-            <span>Dashboard</span>
+            <span>À Propos</span>
         </a>
-    <?php endif; ?>
-
-    <!-- Login -->
-    <?php if (!isset($_SESSION['user_id'])): ?>
-        <a href="/<?php echo $root_folder_name ?>src/views/login.php"
-        <?php if ($page_name == "login.php") echo 'class="active-page"'; ?>>
-            <i class="fa-solid fa-key"></i>
-            <span>Connexion</span>
-        </a>
-    <?php endif; ?>
-
-    <!-- Logout -->
-    <?php if (isset($_SESSION['user_id'])): ?>
-        <a href="/<?php echo $root_folder_name ?>src/views/logout.php"
-        <?php if ($page_name == "logout.php") echo 'class="active-page"'; ?>>
-            <i class="fa-solid fa-right-from-bracket"></i>
-            <span>Déconnexion</span>
-        </a>
-    <?php endif; ?>
-
-    <!-- About us -->
-    <a href="/<?php echo $root_folder_name ?>src/views/about-us.php"
-    <?php if ($page_name == "about-us.php") echo 'class="active-page"'; ?>>
-        <i class="fa-solid fa-circle-info"></i>
-        <span>À Propos</span>
-    </a>
+    </div>
 </div>
